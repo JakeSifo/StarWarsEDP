3e6157ab3565d6fc028bc1c52f981bbffaae9fbf
JakeSifo <bearcoco5@gmail.com>
2023-09-26 16:56:33 -0400

    Added localStorage for films

--- film.js
+++ film.js
@@ -7,13 +7,13 @@ let charactersEl;
 let planetsEl;
 let charactersUl;
 let planetsUl;
+let id;
 const sp = new URLSearchParams(window.location.search);
-
 const baseUrl = `https://swapi2.azurewebsites.net/api`;
 
 // Runs on page load
 addEventListener('DOMContentLoaded', () => {
-  const film = sp.get('id');
+  id = sp.get('id');
   nameH1 = document.querySelector('h1#name');
   prodEl = document.querySelector('span#prod');
   directEl = document.querySelector('span#direct');
@@ -24,40 +24,65 @@ addEventListener('DOMContentLoaded', () => {
   charactersUl = document.querySelector('#characters>ul');
   planetsUl = document.querySelector('#planets>ul');
 
-  getFilm(film);
+  getFilm();
 });
 
-async function getFilm(id) {
+function getLocal(key) {
+  return JSON.parse(window.localStorage.getItem(key));
+}
+function setLocal(key, planets, characters) {
+  let curr = getLocal(key);
+  let filmObj = { id, planets, characters };
+  curr !== null ? window.localStorage.setItem(key, JSON.stringify([...curr, filmObj])) : window.localStorage.setItem(key, JSON.stringify([filmObj]));
+}
+function searchObjectById(arr, targetId) {
+  for (const obj of arr) {
+    if (obj.id === targetId) {
+      return obj;
+    }
+  }
+  return null; // Object not found
+}
+
+async function getFilm() {
   let filmObj;
   try {
-    filmObj = await fetchFilm(id);
-    filmObj.characterObj = await fetchCharacters(id);
-    filmObj.planetsObj = await fetchPlanets(id);
-    // console.log(filmObj);
+    let localFilms = getLocal('films');
+    let thisFilm = localFilms ? searchObjectById(localFilms, id) : null;
+
+    if (thisFilm) {
+      filmObj = thisFilm;
+
+    } else {
+      filmObj = await fetchFilm(id);
+      let characters = await fetchCharacters(id);
+      let planets = await fetchPlanets(id);
+      filmObj.characters = characters;
+      filmObj.planets = planets;
+      setLocal('films', planets, characters);
+    }
+    console.log(filmObj);
   }
   catch (ex) {
     console.error(`Error reading character ${id} data.`, ex.message);
   }
   renderFilm(filmObj);
-
 }
-async function fetchCharacters(id) {
-  let charactersUrl = `${baseUrl}/films/${id}/characters`;
+
+async function fetchCharacters() {
+  const charactersUrl = `${baseUrl}/films/${id}/characters`;
   return await fetch(charactersUrl)
     .then(res => res.json())
 }
-
-async function fetchPlanets(id) {
-  let planetsUrl = `${baseUrl}/films/${id}/planets`;
+async function fetchPlanets() {
+  const planetsUrl = `${baseUrl}/films/${id}/planets`;
   return await fetch(planetsUrl)
     .then(res => res.json())
 }
-
-async function fetchFilm(id) {
+async function fetchFilm() {
   const url = `${baseUrl}/films/${id}`;
-  const filmObj = await fetch(url)
+  return await fetch(url)
     .then(res => res.json())
-  return filmObj;
 }
 
 const renderFilm = film => {
@@ -67,8 +92,8 @@ const renderFilm = film => {
   directEl.textContent = film?.director;
   dateEl.textContent = film?.release_date;
   crawlEl.textContent = film?.opening_crawl;
-  const charactersLis = film?.characterObj?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
+  const charactersLis = film?.characters?.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`)
   charactersUl.innerHTML = charactersLis.join("");
-  const planetsLis = film?.planetsObj?.map(planet => `<li><a href="/planet.html?id=${planet.id}">${planet.name}</li>`)
+  const planetsLis = film?.planets?.map(planet => `<li><a href="/planet.html?id=${planet.id}">${planet.name}</li>`)
   planetsUl.innerHTML = planetsLis.join("");
 }
